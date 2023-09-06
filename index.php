dc4dcbe71d706649e57e774b4585c5d04d5a72dd
endreodev <endreo.dev@gmail.com>
2023-09-06 15:29:30 -0400

    arquivos necessarios

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
-
-
 // inclua antes do código que utilizará o SDK
 require_once(__DIR__ . '../vendor/autoload.php');
+?>
 
-use App\Comunication\Alert;
-
-Alert::sendMessage('Olá Mundo');
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Inclua os links para os arquivos CSS e JavaScript do Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m6 offset-m3">
+                <!-- O card centralizado -->
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title">Fenix Participações</span>
+                        <p>Sistema de integração com D4Sigin.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
