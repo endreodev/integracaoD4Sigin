f1ba1988ba661afff407dcfccce253edcff66a5a
endreodev <endreo.dev@gmail.com>
2023-09-08 09:17:46 -0400

    atualização

---
+++ src/config.exemple.php
@@ -0,0 +1,15 @@
+<?php
+
+/*Dados nescessarios para integração com D4Sigin*/ 
+/** 
+ * uuidSafe = id do cofre que deseja incluir os documentos
+ * 
+*/
+const DATA_D4SIGN_CONFIG = [
+	'host' => "https://secure.d4sign.com.br/api/v1",
+	'tokenAPI' => "live_da50f81a1917",
+	'cryptKey' => "live_crypt_JY",
+	'uuidSafe' => "80b8c278-8ad7-288cef4389be"
+];
+
+
